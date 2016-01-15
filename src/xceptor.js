6d6bdadd8b74ab3b126add88699fd757044ec01f
YanagiEiichi <admin@web-tinker.com>
2016-01-15 12:11:01 +0800

    0.2.5, Use amd2umd

--- src/xceptor.js
+++ src/xceptor.js
@@ -1,30 +1,3 @@
-/**/ void function(scope) {
-/**/ 
-/**/   // CommonJS
-/**/   if (typeof module === 'object' && !!module.exports) return scope(function(name, dependencies, factory) {
-/**/     if(factory === void 0) factory = dependencies, dependencies = [];
-/**/     var args = [];
-/**/     for(var i = 0; i < dependencies.length; i++) args[i] = require(dependencies[i]);
-/**/     module.exports = factory.apply(module.exports, args) || module.exports;
-/**/   });
-/**/ 
-/**/   // AMD, wrap a 'String' to avoid warn of fucking webpack
-/**/   if (String(typeof define) === 'function' && !!define.amd) return scope(define);
-/**/ 
-/**/   // Global
-/**/   scope(function(name, dependencies, factory) {
-/**/     if(factory === void 0) factory = dependencies, dependencies = [];
-/**/     /**/ try { /* Fuck IE8- */
-/**/     /**/   if(typeof execScript === 'object') execScript('var ' + name);
-/**/     /**/ } catch(error) {}
-/**/     window[name] = {}; 
-/**/     var args = [];
-/**/     for(var i = 0; i < dependencies.length; i++) args[i] = window[dependencies[i]];
-/**/     window[name] = factory.apply(window[name], args) || window[name];
-/**/   });
-/**/ 
-/**/ }(function(define) {
-
 /**/ define('XCeptor', function() { /**/
 
 'use strict';
@@ -321,5 +294,3 @@ return XMLHttpRequest.XCeptor = new function() {
 };
 
 /**/ }); /**/
-
-/**/ });
